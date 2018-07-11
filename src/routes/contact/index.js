70c2093d1a70e84ff4521acc5127856367bcdede
ssmith-wombatweb <ssmith@wombatweb.us>
2018-07-10 22:26:10 -0700

    Added missing and invalid value responses for the contact route. Updated details.

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import isValidEmail from 'ssmith-is-valid-email';
 
 dotenv.config();
 
@@ -10,19 +11,17 @@ export const contactDetails = {
       desc: 'Send a contact message to a host site contact.',
       method: 'POST',
       data: {
-        name: {
-          first: 'A string of the first name. Required.',
-          last: 'A string of the last name.',
-        },
+        firstName: 'A string of the first name. Required.',
+        lastName: 'A string of the last name.',
         senderEmail: 'A string of the sender email. Must be a valid email. Required.',
         receiverEmail: 'A string of the receiver email. Must be a valid email. Required.',
         subject: 'A string of the message from the sender. Required.',
         msg: 'A string of the message from the sender. Required.',
         confirmation: 'A string of a confirmation message to be sent back to the sender. Will not send confirmation message if missing.',
-        greeting: 'A string with the greeting to the sender. Will be formatted like so "$greeting $firstName $lastName,"',
-        receptionMsg: 'A string of a reception message to be sent to the receiver. If missing only the msg will be sent with the contact info.',
-        receiverName: 'A string of the receivers name. Required',
-        signOff: 'A string with the sign off response to the sender and receiver. Will be formatted like so "$signOff, $receiverName"',
+        greeting: 'A string with the greeting to the sender. Will be formatted like so "$greeting $firstName $lastName,". If missing will use "Hello".',
+        receptionMsg: 'A string of a intro reception message to be sent to the receiver. If missing only the msg will be sent with the contact info.',
+        receiverName: 'A string of the receivers name. Required.',
+        signOff: 'A string with the sign off response to the sender and receiver. Will be formatted like so "$signOff, $receiverName". If missing will use "Thank you,"',
       },
     },
   },
@@ -30,25 +29,74 @@ export const contactDetails = {
 
 function get(req, res) {
   contactDetails.error = 'There is no data to GET from this route. Details of this route have been included.';
-  return res.status(404).json(contactDetails);
+  return res.status(200).json(contactDetails);
 }
 
 function post(req, res) {
   const { EMAIL_PASS } = process.env;
-  console.log(req.body);
 
   const {
-    name,
+    firstName,
+    lastName,
     senderEmail,
     receiverEmail,
     subject,
     msg,
     confirmation,
-    greeting,
+    greeting = 'Hello',
     receptionMsg,
     receiverName,
-    signOff,
+    signOff = 'Thank you',
   } = req.body;
+
+  if (!firstName) {
+    return res.status(400).json({
+      msg: 'The first name is missing.',
+      reqBody: req.body,
+    });
+  }
+  if (!senderEmail) {
+    return res.status(400).json({
+      msg: 'The sender email is missing.',
+      reqBody: req.body,
+    });
+  }
+  if (!isValidEmail(senderEmail)) {
+    return res.status(400).json({
+      msg: 'The sender email is invalid.',
+      reqBody: req.body,
+    });
+  }
+  if (!receiverEmail) {
+    return res.status(400).json({
+      msg: 'The receiver email is missing.',
+      reqBody: req.body,
+    });
+  }
+  if (!isValidEmail(receiverEmail)) {
+    return res.status(400).json({
+      msg: 'The receiver email is invalid.',
+      reqBody: req.body,
+    });
+  }
+  if (!subject) {
+    return res.status(400).json({
+      msg: 'The subject is missing.',
+      reqBody: req.body,
+    });
+  }
+  if (!msg) {
+    return res.status(400).json({
+      msg: 'The message is missing.',
+      reqBody: req.body,
+    });
+  }
+  if (!receiverName) {
+    return res.status(400).json({
+      msg: 'The receiver name is missing.',
+      reqBody: req.body,
+    });
+  }
 
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -70,11 +118,13 @@ function post(req, res) {
     text: `\
 ${greeting} ${receiverName},
 
-${receptionMsg}
-${msg}
+${receptionMsg
+    ? `${receptionMsg}
+${msg}`
+    : msg}
 
 ${signOff},
-${name.first} ${name.last}
+${firstName}${lastName ? ` ${lastName}` : ''}
 `,
   };
 
@@ -83,17 +133,76 @@ ${name.first} ${name.last}
     to: senderEmail,
     subject,
     text: `\
-${greeting} ${name.first} ${name.last},
+${greeting} ${firstName}${lastName ? ` ${lastName}` : ''},
 
-${confirmation}
-${msg}
+${confirmation
+    ? `${confirmation}
+${msg}`
+    : msg}
 
 ${signOff},
 ${receiverName}
 `,
   };
 
-  return res.status(200).send(receiverMessage);
+  return transporter.sendMail(
+    receiverMessage,
+    (receiverError, receiverInfo) => {
+      let receiverMsgResponse;
+      if (receiverError) {
+        receiverMsgResponse = {
+          msg: `Your contact request failed. Please email ${receiverEmail} directly.`,
+          code: receiverError.responseCode,
+          error: receiverError,
+        };
+        return res.status(receiverMsgResponse.code).json(receiverMsgResponse);
+      }
+
+      const newReceiverInfo = receiverInfo;
+      newReceiverInfo.msg = receiverMessage;
+
+      if (confirmation) {
+        return transporter.sendMail(
+          confirmationMsg,
+          (confirmError, confirmInfo) => {
+            let confirmMsgResponse;
+
+            if (confirmError) {
+              confirmMsgResponse = {
+                msg: 'You message was sent but the confirmation message failed.',
+                code: confirmError.responseCode,
+                error: confirmError,
+                receiverInfo: newReceiverInfo,
+              };
+              return res.status(confirmMsgResponse.code).json(confirmMsgResponse);
+            }
+
+            const newConfirmInfo = confirmInfo;
+            newConfirmInfo.msg = confirmationMsg;
+
+            confirmMsgResponse = {
+              msg: 'Your message was successfully sent.',
+              code: 200,
+              info: {
+                newConfirmInfo,
+                newReceiverInfo,
+              },
+            };
+
+            return res.status(confirmMsgResponse.code).json(confirmMsgResponse);
+          },
+        );
+      }
+
+      return res.status(200).json({
+        msg: 'Your message was successfully sent.',
+        code: 200,
+        info: {
+          newReceiverInfo,
+        },
+      });
+    },
+  );
 }
 
 function Contact(express) {
